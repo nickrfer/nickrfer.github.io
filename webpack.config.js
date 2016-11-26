f19deb964a5694977abc5d1f8bb5a3ada63e7e7a
Nicolas Ferreira <nickrfer@gmail.com>
2016-11-26 21:28:11 -0200

    First blog post!

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: './webpack/entry.js',
   output: {
     // we’re going to put the generated file in the assets folder so jekyll will grab it.
-    path: 'src/assets/javascripts/',
+    path: './js/',
     filename: 'bundle.js'
   },
   module: {
--- webpack/entry.js
+++ webpack/entry.js
@@ -1,1 +1,11 @@
-console.log("Hello, nickrfer!")
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+import Hello from './components/Hello';
+class App extends Component {
+ render() {
+ return (
+ <Hello />
+ )
+ }
+}
+render(<App />, document.getElementById('root'));
